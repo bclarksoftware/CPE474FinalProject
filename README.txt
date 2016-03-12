a936de013e2694d57b0447f665dd85c735997fb4
bclarksoftware <bclarksoftware@gmail.com>
2016-03-12 11:54:39 -0800

    Initial commit... which is broken because of the sound.

---
+++ README.txt
@@ -0,0 +1,18 @@
+Brandon Clark
+CPE 474
+
+=== Chicken Disco Dancer ===
+
+HOW TO BUILD:
+- Open project root directory.
+- mkdir build
+- cd build
+- cmake ..
+- make -j8
+- ./FinalProject
+
+Technology included:
+- Music
+- Skinned animations
+- Cloth simulation
+- Collision detection between skinned character and cloth
---
+++ src/Cloth.cpp
@@ -0,0 +1,407 @@
+#include <iostream>
+
+#include "Cloth.h"
+#include "Particle.h"
+#include "Spring.h"
+#include "MatrixStack.h"
+#include "Program.h"
+#include "GLSL.h"
+
+using namespace std;
+using namespace Eigen;
+
+shared_ptr<Spring> createSpring(const shared_ptr<Particle> p0, const shared_ptr<Particle> p1, double E)
+{
+	auto s = make_shared<Spring>(p0, p1);
+	s->E = E;
+	Vector3d x0 = p0->x;
+	Vector3d x1 = p1->x;
+	Vector3d dx = x1 - x0;
+	s->L = dx.norm();
+	return s;
+}
+
+Cloth::Cloth(int rows, int cols,
+			 const Vector3d &x00,
+			 const Vector3d &x01,
+			 const Vector3d &x10,
+			 const Vector3d &x11,
+			 double mass,
+			 double stiffness,
+			 const Vector2d &damping)
+{
+	assert(rows > 1);
+	assert(cols > 1);
+	assert(mass > 0.0);
+	assert(stiffness > 0.0);
+	
+	this->rows = rows;
+	this->cols = cols;
+	this->damping = damping;
+	
+	// Create particles
+	n = 0;
+	double r = 0.02; // Used for collisions
+	int nVerts = rows*cols;
+	for(int i = 0; i < rows; ++i) {
+		double u = i / (rows - 1.0);
+		Vector3d x0 = (1 - u)*x00 + u*x10;
+		Vector3d x1 = (1 - u)*x01 + u*x11;
+		for(int j = 0; j < cols; ++j) {
+			double v = j / (cols - 1.0);
+			Vector3d x = (1 - v)*x0 + v*x1;
+			auto p = make_shared<Particle>();
+			particles.push_back(p);
+			p->r = r;
+			p->x = x;
+			p->v << 0.0, 0.0, 0.0;
+			p->m = mass/(nVerts);
+			// Pin two particles
+			if(i == 0 && (j == 0 || j == cols-1)) {
+				p->fixed = true;
+				p->i = -1;
+			} else {
+				p->fixed = false;
+				p->i = n;
+				n += 3;
+			}
+		}
+	}
+	
+	// Create x springs
+	for(int i = 0; i < rows; ++i) {
+		for(int j = 0; j < cols-1; ++j) {
+			int k0 = i*cols + j;
+			int k1 = k0 + 1;
+			springs.push_back(createSpring(particles[k0], particles[k1], stiffness));
+		}
+	}
+	
+	// Create y springs
+	for(int j = 0; j < cols; ++j) {
+		for(int i = 0; i < rows-1; ++i) {
+			int k0 = i*cols + j;
+			int k1 = k0 + cols;
+			springs.push_back(createSpring(particles[k0], particles[k1], stiffness));
+		}
+	}
+	
+	// Create shear springs
+	for(int i = 0; i < rows-1; ++i) {
+		for(int j = 0; j < cols-1; ++j) {
+			int k00 = i*cols + j;
+			int k10 = k00 + 1;
+			int k01 = k00 + cols;
+			int k11 = k01 + 1;
+			springs.push_back(createSpring(particles[k00], particles[k11], stiffness));
+			springs.push_back(createSpring(particles[k10], particles[k01], stiffness));
+		}
+	}
+
+	// Build system matrices and vectors
+	M.resize(n,n);
+	K.resize(n,n);
+	v.resize(n);
+	f.resize(n);
+	
+	// Build vertex buffers
+	posBuf.clear();
+	norBuf.clear();
+	texBuf.clear();
+	eleBuf.clear();
+	posBuf.resize(nVerts*3);
+	norBuf.resize(nVerts*3);
+	updatePosNor();
+	// Texture coordinates (don't change)
+	for(int i = 0; i < rows; ++i) {
+		for(int j = 0; j < cols; ++j) {
+			texBuf.push_back(i/(rows-1.0));
+			texBuf.push_back(j/(cols-1.0));
+		}
+	}
+	// Elements (don't change)
+	for(int i = 0; i < rows-1; ++i) {
+		for(int j = 0; j < cols; ++j) {
+			int k0 = i*cols + j;
+			int k1 = k0 + cols;
+			// Triangle strip
+			eleBuf.push_back(k0);
+			eleBuf.push_back(k1);
+		}
+	}
+}
+
+Cloth::~Cloth()
+{
+}
+
+void Cloth::tare()
+{
+	for(int k = 0; k < (int)particles.size(); ++k) {
+		particles[k]->tare();
+	}
+}
+
+void Cloth::reset()
+{
+	for(int k = 0; k < (int)particles.size(); ++k) {
+		particles[k]->reset();
+	}
+	updatePosNor();
+}
+
+void Cloth::updatePosNor()
+{
+	// Position
+	for(int i = 0; i < rows; ++i) {
+		for(int j = 0; j < cols; ++j) {
+			int k = i*cols + j;
+			Vector3d x = particles[k]->x;
+			posBuf[3*k+0] = x(0);
+			posBuf[3*k+1] = x(1);
+			posBuf[3*k+2] = x(2);
+		}
+	}
+	// Normal
+	for(int i = 0; i < rows; ++i) {
+		for(int j = 0; j < cols; ++j) {
+			// Each particle has four neighbors
+			//
+			//      v1
+			//     /|\
+			// u0 /_|_\ u1
+			//    \ | /
+			//     \|/
+			//      v0
+			//
+			// Use these four triangles to compute the normal
+			int k = i*cols + j;
+			int ku0 = k - 1;
+			int ku1 = k + 1;
+			int kv0 = k - cols;
+			int kv1 = k + cols;
+			Vector3d x = particles[k]->x;
+			Vector3d xu0, xu1, xv0, xv1, dx0, dx1, c;
+			Vector3d nor(0.0, 0.0, 0.0);
+			int count = 0;
+			// Top-right triangle
+			if(j != cols-1 && i != rows-1) {
+				xu1 = particles[ku1]->x;
+				xv1 = particles[kv1]->x;
+				dx0 = xu1 - x;
+				dx1 = xv1 - x;
+				c = dx0.cross(dx1);
+				nor += c.normalized();
+				++count;
+			}
+			// Top-left triangle
+			if(j != 0 && i != rows-1) {
+				xu1 = particles[kv1]->x;
+				xv1 = particles[ku0]->x;
+				dx0 = xu1 - x;
+				dx1 = xv1 - x;
+				c = dx0.cross(dx1);
+				nor += c.normalized();
+				++count;
+			}
+			// Bottom-left triangle
+			if(j != 0 && i != 0) {
+				xu1 = particles[ku0]->x;
+				xv1 = particles[kv0]->x;
+				dx0 = xu1 - x;
+				dx1 = xv1 - x;
+				c = dx0.cross(dx1);
+				nor += c.normalized();
+				++count;
+			}
+			// Bottom-right triangle
+			if(j != cols-1 && i != 0) {
+				xu1 = particles[kv0]->x;
+				xv1 = particles[ku1]->x;
+				dx0 = xu1 - x;
+				dx1 = xv1 - x;
+				c = dx0.cross(dx1);
+				nor += c.normalized();
+				++count;
+			}
+			nor /= count;
+			nor.normalize();
+			norBuf[3*k+0] = nor(0);
+			norBuf[3*k+1] = nor(1);
+			norBuf[3*k+2] = nor(2);
+		}
+	}
+}
+
+void Cloth::step(double h, const Vector3d &grav, const vector< shared_ptr<Particle> > spheres)
+{
+	M.setZero();
+	K.setZero();
+	v.setZero();
+	f.setZero();
+
+    // Set v, f, and M
+    for(int k = 0; k < (int)particles.size(); ++k)
+    {
+        if (!particles[k]->fixed)
+        {
+            // Set Velocity
+            v.block<3,1>(particles[k]->i, 0) = particles[k]->v;
+            
+            // Set Force with gravity
+            f.block<3,1>(particles[k]->i, 0) = particles[k]->m * grav;
+            
+            // Fill in M matrix
+            M.block<3,3>(particles[k]->i, particles[k]->i) = particles[k]->m * Matrix3d::Identity();
+        }
+    }
+    
+    // Update f with spring forces.
+    for(int k = 0; k < (int)springs.size(); ++k)
+    {
+        Vector3d deltaX = springs[k]->p1->x - springs[k]->p0->x;
+        double springLength = deltaX.norm();
+        
+        Vector3d fs = springs[k]->E * (springLength - springs[k]->L) * (deltaX / springLength);
+        
+//        Matrix3d test = deltaX * deltaX.transpose();
+        
+//        double test = deltaX.transpose().dot(deltaX);
+        
+        Matrix3d ks = (springs[k]->E/(springLength * springLength))
+        * (((1.0 - (springLength - springs[k]->L)/springLength) * Matrix3d(deltaX * deltaX.transpose()))
+            + (((springLength - springs[k]->L) / springLength) * ((double)(deltaX.transpose() * deltaX)
+                                                              * Matrix3d::Identity())));
+        
+        int ndx0 = springs[k]->p0->i;
+        int ndx1 = springs[k]->p1->i;
+        
+        if (!springs[k]->p0->fixed)
+        {
+            f.segment<3>(springs[k]->p0->i) += fs;
+        }
+        
+        if (!springs[k]->p1->fixed)
+        {
+            f.segment<3>(springs[k]->p1->i) += -fs;
+        }
+        
+        if (!springs[k]->p0->fixed && !springs[k]->p1->fixed)
+        {
+            K.block<3,3>(ndx0, ndx0) += ks;
+            K.block<3,3>(ndx0, ndx1) += -ks;
+            K.block<3,3>(ndx1, ndx0) += -ks;
+            K.block<3,3>(ndx1, ndx1) += ks;
+        }
+        else if (!springs[k]->p0->fixed)
+        {
+            K.block<3,3>(ndx0, ndx0) += ks;
+        }
+        else if (!springs[k]->p1->fixed)
+        {
+            K.block<3,3>(ndx1, ndx1) += ks;
+        }
+    }
+    
+    MatrixXd D = damping[0] * h * M + damping[1] * h * h * K;
+    
+    MatrixXd A = M + D;
+    VectorXd b = M * v + h * f;
+    
+    v = A.ldlt().solve(b);
+	
+    // Update particle velocity and position.
+    for(int k = 0; k < (int)particles.size(); ++k)
+    {
+        if (!particles[k]->fixed)
+        {
+            particles[k]->v = v.segment<3>(particles[k]->i);
+            particles[k]->x += h * particles[k]->v;
+        }
+    }
+    
+    for(int k = 0; k < (int)particles.size(); ++k)
+    {
+        if (!particles[k]->fixed)
+        {
+            Vector3d dX = particles[k]->x - spheres[0]->x;
+            
+            if (dX.norm() <= (particles[k]->r + spheres[0]->r))
+            {
+                Vector3d xc = spheres[0]->x + ((spheres[0]->r + particles[k]->r) * dX.normalized());
+                Vector3d np = xc - particles[k]->x;
+                particles[k]->x = xc;
+                
+                particles[k]->v = particles[k]->v - (((double)(particles[k]->v.transpose() * np.normalized())) * np.normalized());
+                
+                particles[k]->v += spheres[0]->v.dot(np.normalized()) * np.normalized();
+            }
+        }
+    }
+    
+	// Update position and normal buffers
+	updatePosNor();
+}
+
+void Cloth::init()
+{
+	glGenBuffers(1, &posBufID);
+	glBindBuffer(GL_ARRAY_BUFFER, posBufID);
+	glBufferData(GL_ARRAY_BUFFER, posBuf.size()*sizeof(float), &posBuf[0], GL_DYNAMIC_DRAW);
+	
+	glGenBuffers(1, &norBufID);
+	glBindBuffer(GL_ARRAY_BUFFER, norBufID);
+	glBufferData(GL_ARRAY_BUFFER, norBuf.size()*sizeof(float), &norBuf[0], GL_DYNAMIC_DRAW);
+	
+	glGenBuffers(1, &texBufID);
+	glBindBuffer(GL_ARRAY_BUFFER, texBufID);
+	glBufferData(GL_ARRAY_BUFFER, texBuf.size()*sizeof(float), &texBuf[0], GL_STATIC_DRAW);
+	
+	glGenBuffers(1, &eleBufID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eleBufID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, eleBuf.size()*sizeof(unsigned int), &eleBuf[0], GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
+	assert(glGetError() == GL_NO_ERROR);
+}
+
+void Cloth::draw(shared_ptr<MatrixStack> MV, const shared_ptr<Program> p) const
+{
+	// Draw mesh
+	glUniform3fv(p->getUniform("kdFront"), 1, Vector3f(1.0, 0.0, 0.0).data());
+	glUniform3fv(p->getUniform("kdBack"),  1, Vector3f(1.0, 1.0, 0.0).data());
+	MV->pushMatrix();
+	glUniformMatrix4fv(p->getUniform("MV"), 1, GL_FALSE, MV->topMatrix().data());
+    
+	int h_pos = p->getAttribute("vertPos");
+	GLSL::enableVertexAttribArray(h_pos);
+	glBindBuffer(GL_ARRAY_BUFFER, posBufID);
+	glBufferData(GL_ARRAY_BUFFER, posBuf.size()*sizeof(float), &posBuf[0], GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(h_pos, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+    
+	int h_nor = p->getAttribute("vertNor");
+	GLSL::enableVertexAttribArray(h_nor);
+	glBindBuffer(GL_ARRAY_BUFFER, norBufID);
+	glBufferData(GL_ARRAY_BUFFER, norBuf.size()*sizeof(float), &norBuf[0], GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(h_nor, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+    
+	int h_tex = p->getAttribute("vertTex");
+	GLSL::enableVertexAttribArray(h_tex);
+	glBindBuffer(GL_ARRAY_BUFFER, texBufID);
+    
+	glVertexAttribPointer(h_tex, 2, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eleBufID);
+    
+	for(int i = 0; i < rows; ++i) {
+		glDrawElements(GL_TRIANGLE_STRIP, 2*cols, GL_UNSIGNED_INT, (const void *)(2*cols*i*sizeof(unsigned int)));
+	}
+    
+	GLSL::disableVertexAttribArray(h_tex);
+	GLSL::disableVertexAttribArray(h_nor);
+	GLSL::disableVertexAttribArray(h_pos);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	MV->popMatrix();
+}
